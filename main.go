4617493dc41cc01215be1745d17a36559fd5acc5
simba-fs <simba.fs@gmail.com>
2022-02-19 22:47:39 +0800

    Integrate bot into cmd

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"time"
 
 	// "github.com/simba-fs/gitdiary/note"
+	"github.com/simba-fs/gitdiary/bot"
 
 	"github.com/cristalhq/aconfig"
 	"github.com/cristalhq/aconfig/aconfigtoml"
@@ -37,26 +38,31 @@ func init() {
 func main() {
 	fmt.Println(os.Args[1:], config)
 
-	// convert date from string to int
-	var date []int
-	for _, v := range os.Args[1:] {
-		i, err := strconv.Atoi(v)
-		if err != nil {
-			fmt.Println(err)
-			os.Exit(1)
+	if len(os.Args) > 1 && os.Args[1] == "bot" {
+		fmt.Println("start bot")
+		bot.Run()
+	} else {
+		// convert date from string to int
+		var date []int
+		for _, v := range os.Args[1:] {
+			i, err := strconv.Atoi(v)
+			if err != nil {
+				fmt.Println(err)
+				os.Exit(1)
+			}
+			date = append(date, i)
 		}
-		date = append(date, i)
-	}
-	fmt.Println(date)
+		fmt.Println(date)
 
-	year, month, day := time.Now().Date()
-	switch len(date) {
-	case 3:
-		year, month, day = date[0], time.Month(date[1]), date[2]
-	case 2:
-		month, day = time.Month(date[0]), date[1]
-	case 1:
-		day = date[0]
+		year, month, day := time.Now().Date()
+		switch len(date) {
+		case 3:
+			year, month, day = date[0], time.Month(date[1]), date[2]
+		case 2:
+			month, day = time.Month(date[0]), date[1]
+		case 1:
+			day = date[0]
+		}
+		fmt.Println(year, month, day)
 	}
-	fmt.Println(year, month, day)
 }
