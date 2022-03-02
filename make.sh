76986a902d1d685740948b98935fcebbb20bdc39
simba-fs <simba.fs@gmail.com>
2022-03-03 00:54:09 +0800

    update make.sh

--- make.sh
+++ make.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
 case $1 in 
-	"release")
+	"release"|*)
 		if [ -z "$2" ]; then
 			echo -n "Enter release version: "
 			read VERSION
 		fi
 		VERSION=${2:-$VERSION}
 		echo "Releasing version $VERSION ..."
-		git tag -as $VERSION -m "Release $VERSION"
+		git tag -as $VERSION 
 		git push origin --tags
+		go list -m github.com/simba-fs/telegrary@$VERSION
 		;;
 esac
